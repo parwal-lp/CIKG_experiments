f9f292603d1f4185caf943b96bd07290eefb20dd
laurapapi <parwal.lp@gmail.com>
2025-05-12 21:59:16 +0200

    corretto classificatore even

--- training-script.py
+++ training-script.py
@@ -10,9 +10,9 @@ device = detectDevice()
 #train all models
 #SLPmodels = trainModels('SLP', train_loader, device)
 #MLPmodels = trainModels('MLP', train_loader, device)
-SLPeven = trainMultinumClassifier('SLP', train_loader, [2,4,6,8], device)
+SLPeven = trainMultinumClassifier('SLP', train_loader, [0,2,4,6,8], device)
 
 #test all models
 #testModels(SLPmodels, test_loader, device)
 #testModels(MLPmodels, test_loader, device)
-test(test_loader, SLPeven, [2,4,6,8], device)
+test(test_loader, SLPeven, [0,2,4,6,8], device)
