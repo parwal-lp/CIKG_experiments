89ba46fcd01c169eddd610fcbac44cf425268e4d
parwal-google-high-cpu <parwal@google-high-cpu.com>
2026-02-23 17:36:59 +0000

    fatto encoding di rete unica a partire da query in onnx

--- marabou-solver.py
+++ marabou-solver.py
@@ -3,6 +3,10 @@ import numpy as np
 
 from maraboupy import Marabou
 from maraboupy import MarabouCore
+from maraboupy.Marabou import createOptions
+
+'''
+# NNET version
 
 nnet0 = "codifica_0.nnet"
 nnet1 = "codifica_1.nnet"
@@ -10,23 +14,37 @@ nnet1 = "codifica_1.nnet"
 network0 = Marabou.read_nnet(nnet0)
 network1 = Marabou.read_nnet(nnet1)
 
-vals0, stats0 = network0.solve()
-vals1, stats1 = network1.solve()
+options = createOptions()
 
-print("Network 0 vals:", vals0)
-print("Network 0 stats:", stats0)
-print("Network 1 vals:", vals1)
-print("Network 1 stats:", stats1)
+ret = network0.solve()
+#vals1, stats1 = network1.solve()
 
+print("Network 0 result:", ret[0])
+print("Network 0 solution:", ret[1])
+print("Network 0 stats", ret[2])
+#print("Network 0 numsplits", ret[2].getNumSplits())
+#print("Network 0 totaltime", ret[2].getTotalTime())
+#print("Network 1 vals:", vals1)
+#print("Network 1 stats:", stats1)
 
+query0 = network0.getInputQuery()
+query1 = network1.getInputQuery()
 
-# query = MarabouCore.InputQuery()
-# query.setNumberOfVariables(28*28)
-# #query0 = MarabouCore.InputQuery()
-# MarabouCore.createInputQuery(query, nnet0, '')
+'''
 
-# query.addEquation(query0)
+# ONNX version
+network = Marabou.read_onnx("codifica_0and1.onnx")
 
-# ret = query.solve()
+inputVars = network.inputVars[0].flatten()
+outputVars = network.outputVars[0].flatten()
 
-# print(ret)
+for i in range(len(inputVars)):
+    network.setLowerBound(inputVars[i], 0.0)
+    network.setUpperBound(inputVars[i], 1.0)
+
+network.setLowerBound(outputVars[0], 0)
+
+result = network.solve()
+print(result[0])
+print(result[1])
+print(result[2])
