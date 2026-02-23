da1576ce4b30d1f56e47dcbee5d6bd8cbcf037d0
parwal-lp <parwal.lp@gmail.com>
2026-02-23 11:35:40 +0100

    prove marabou

--- marabou-solver.py
+++ marabou-solver.py
@@ -1,16 +1,32 @@
 import sys
 import numpy as np
 
-## %
-# Path to Marabou folder if you did not export it
-
-# sys.path.append('/home/USER/git/Marabou')
 from maraboupy import Marabou
+from maraboupy import MarabouCore
 
 nnet0 = "codifica_0.nnet"
 nnet1 = "codifica_1.nnet"
 
-encoding_0 = Marabou.read_nnet(nnet0)
-encoding_1 = Marabou.read_nnet(nnet1)
+network0 = Marabou.read_nnet(nnet0)
+network1 = Marabou.read_nnet(nnet1)
 
-vals1, stats1 = encoding_1.solve()
+vals0, stats0 = network0.solve()
+vals1, stats1 = network1.solve()
+
+print("Network 0 vals:", vals0)
+print("Network 0 stats:", stats0)
+print("Network 1 vals:", vals1)
+print("Network 1 stats:", stats1)
+
+
+
+# query = MarabouCore.InputQuery()
+# query.setNumberOfVariables(28*28)
+# #query0 = MarabouCore.InputQuery()
+# MarabouCore.createInputQuery(query, nnet0, '')
+
+# query.addEquation(query0)
+
+# ret = query.solve()
+
+# print(ret)
